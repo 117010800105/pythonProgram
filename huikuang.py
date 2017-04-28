3394ce5931a0b0ea80577dde929932d20cfc41aa
Weizhong Lin <javafalcon@163.com>
2017-04-28 21:47:58 +0800

    initial project
    initial

---
+++ huikuang.py
@@ -0,0 +1,21 @@
+import turtle
+import time
+length = 400
+r=0.9
+turtle.setup(500,500,200,200)
+turtle.penup()
+turtle.goto(-200,-200)
+turtle.pendown()
+turtle.right(270)
+while length > 20:
+    turtle.fd(length)
+    turtle.right(90)
+    turtle.fd(length)
+    turtle.right(90)
+    turtle.fd(length*r)
+    turtle.right(90)
+    turtle.fd(length*r)
+    turtle.right(90)
+    length = length*r*r
+    
+
